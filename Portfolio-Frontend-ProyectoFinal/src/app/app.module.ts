d95e3407b1cea049274f582b654802e6a4340c3c
Naimgisse <naimjara02@gmail.com>
2023-01-14 23:49:56 -0300

    Header y Banner

--- Portfolio-Frontend-ProyectoFinal/src/app/app.module.ts
+++ Portfolio-Frontend-ProyectoFinal/src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { LogoAPComponent } from "./components/logo-ap/logo-ap.component";
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LogoAPComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        LogoAPComponent,
+        SocialComponent,
+        BannerComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ]
 })
 export class AppModule { }
