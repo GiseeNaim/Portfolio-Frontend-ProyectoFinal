272c186ef4c21a6f992ba8329c8c88cc7a9a30b9
Naimgisse <109824555+GiseeNaim@users.noreply.github.com>
2023-03-13 17:27:15 -0300

    FinalFrontEnd

--- Portfolio-Frontend-ProyectoFinal/src/app/app.module.ts
+++ Portfolio-Frontend-ProyectoFinal/src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from "./components/logo-ap/logo-ap.component";
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [
@@ -15,12 +22,19 @@ import { BannerComponent } from './components/banner/banner.component';
         LogoAPComponent,
         SocialComponent,
         BannerComponent,
+        AcercaDeComponent,
+        ExperienciaComponent,
+        EducacionComponent,
+        HysComponent,
+        ProyectoComponent,
+        FooterComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        NgCircleProgressModule.forRoot({})
     ]
 })
 export class AppModule { }
